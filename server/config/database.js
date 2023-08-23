d35889c36839f34ce87ac0547a1c1dfac56ab892
bayanicruz <bayani.cruz@gmail.com>
2023-08-23 23:40:42 +1000

    Initial commit

---
+++ server/config/database.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const dot = require("dotenv").config({path: './config/.env'});
+
+const connectDatabase = async () => {
+  const mongoUrl = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority`;
+  console.log(mongoUrl)
+  mongoose
+    .connect(mongoUrl)
+    .then(() => console.log(`Connected to MongoDB`))
+    .catch((err) =>
+      console.error(
+        "Error connecting to MongoDB:",
+        err.message,
+      ),
+    );
+};
+
+module.exports = connectDatabase;
