285b8ee9ebb831fbab989d641def44f361e564e3
bayanicruz <bayani.cruz@gmail.com>
2023-08-25 18:59:40 +1000

    Initial build for search

--- client/src/App.js
+++ client/src/App.js
@@ -10,16 +10,24 @@ import List from "@mui/material/List";
 import ListItem from "./components/ListItem";
 
 function App() {
-  const contacts = [{ Name: "John Doe" }, { Name: "Jane Doe" }, { Name: "John Doe La Cruz" }];
+  const [contactList, setContactList] = React.useState([
+    { Name: "John Doe" },
+    { Name: "Jane Doe" },
+    { Name: "John Doe La Cruz" },
+  ]);
+  
+  const handleSearch = () => {
+    //do something
+  };
 
   return (
     <Container>
       <Paper elevation={10} sx={{ p: 3, mt: 3 }}>
         <Box sx={{ mx: "auto" }}>
           <h3>Contact Management</h3>
-          <TextField id="outlined-basic" label="Search" variant="outlined" sx={{ width: "100%" }} />
+          <TextField id="outlined-basic" label="Search" variant="outlined" sx={{ width: "100%" }} onChange={handleSearch}/>
           <List sx={{ width: "100%", bgcolor: "background.paper" }}>
-            {contacts.map((object) => (
+            {contactList.map((object) => (
               <ListItem contact={object} />
             ))}
           </List>
