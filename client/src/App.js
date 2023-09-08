ce47b584b3c2cc3fe8e476277ed8adcd6d761ca9
bayanicruz <64904982+bayanicruz@users.noreply.github.com>
2023-09-08 19:35:49 +1000

    Add drawer to create new contacts (#2)
    * Add BottomDrawer to create new contacts
    
    * Add contact creation ui
    
    * Add new contact
    
    * Use toggleDrawer for Add button

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,13 @@ import * as React from "react";
 import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
 
 import TextField from "@mui/material/TextField";
 import List from "@mui/material/List";
 
 import ListItem from "./components/ListItem";
+import BottomDrawer from "./components/BottomDrawer";
 
 function App() {
   const contacts = [
@@ -19,15 +21,29 @@ function App() {
   const [contactList, setSearchList] = React.useState(contacts);
 
   const handleSearch = (value) => {
-    let contactList = contacts.filter((e) => e.Name.toUpperCase().includes(value.toUpperCase()));
+    let contactList = contacts.filter((e) =>
+      e.Name.toUpperCase().includes(value.toUpperCase()),
+    );
     setSearchList(contactList);
+  };
+
+  const handleAddContact = (value) => {
+    setSearchList([...contactList, { Name: value }]);
   };
 
   return (
     <Container>
       <Paper elevation={10} sx={{ p: 3, mt: 3 }}>
         <Box sx={{ mx: "auto" }}>
-          <h3>Contact Management</h3>
+          <Grid container spacing={1}>
+            <Grid item xs={9}>
+              <h3>Contact Management</h3>
+            </Grid>
+            <Grid item xs={3}>
+              <BottomDrawer createContact={handleAddContact} />
+            </Grid>
+          </Grid>
+
           <TextField
             id="outlined-basic"
             label="Search"
---
+++ client/src/components/BottomDrawer.js
@@ -0,0 +1,93 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import Button from "@mui/material/Button";
+
+import AddBoxIcon from "@mui/icons-material/AddBox";
+import Grid from "@mui/material/Grid";
+import TextField from "@mui/material/TextField";
+
+export default function BottomDrawer({ createContact }) {
+  const [state, setState] = React.useState(false);
+
+  const [name, setName] = React.useState();
+
+  const toggleDrawer = (open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setState(open);
+  };
+
+  const list = () => (
+    <Box sx={{ width: "auto", height: 850 }} role="presentation">
+      <Grid container>
+        <Grid item xs={4}>
+          <Button
+            variant="text"
+            onClick={toggleDrawer(false)}
+            onKeyDown={toggleDrawer(false)}
+          >
+            Cancel
+          </Button>
+        </Grid>
+        <Grid item xs={4}>
+          <Box sx={{ textAlign: "center" }}>
+            <h3>New Contact</h3>
+          </Box>
+        </Grid>
+        <Grid item xs={4}>
+          <Box sx={{ float: "right" }}>
+            <Button variant="text" onClick={(e) => {
+              createContact(name)
+              toggleDrawer(false)(e)
+            }}>
+              Add
+            </Button>
+          </Box>
+        </Grid>
+      </Grid>
+      <Divider />
+      <List>
+        <ListItem disablePadding>
+          <TextField
+            id="outlined-basic"
+            label="Name"
+            variant="outlined"
+            sx={{ width: "100%", m: 2 }}
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </ListItem>
+      </List>
+    </Box>
+  );
+
+  return (
+    <div>
+      {
+        // <React.Fragment key={anchor}> for loop?
+        <Box sx={{ float: "right" }}>
+          <IconButton
+            aria-label="delete"
+            size="large"
+            onClick={toggleDrawer(true)}
+          >
+            <AddBoxIcon fontSize="inherit" />
+          </IconButton>
+          <Drawer anchor={"bottom"} open={state} onClose={toggleDrawer(false)}>
+            {list()}
+          </Drawer>
+        </Box>
+      }
+    </div>
+  );
+}
